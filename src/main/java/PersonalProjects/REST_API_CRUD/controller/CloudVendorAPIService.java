ec6d291786b7de6314f054c20a0c04b038803327
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2024-07-01 16:41:04 -0700

    created controller API service with GET and POST mappings, created Cloud Vendor class

---
+++ src/main/java/PersonalProjects/REST_API_CRUD/controller/CloudVendorAPIService.java
@@ -0,0 +1,31 @@
+package PersonalProjects.REST_API_CRUD.controller;
+
+import PersonalProjects.REST_API_CRUD.model.CloudVendor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/cloundvendor")
+public class CloudVendorAPIService {
+
+  CloudVendor cloudVendor;
+  @GetMapping("{vendorId}")
+  public CloudVendor getCloudVendorDetails(String vendorId){
+
+    return cloudVendor;
+//        new CloudVendor(
+//        "C1", "Vendor 1",
+//        "Address One", "xxxxx");
+  }
+
+  @PostMapping
+  public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+    this.cloudVendor = cloudVendor;
+    return "Cloud Vendor Created Successfully";
+  }
+
+}
