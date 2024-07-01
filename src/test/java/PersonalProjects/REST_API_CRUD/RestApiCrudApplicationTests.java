1e7f827cb816150e600b78bf28ede6805d06c09f
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2024-06-30 23:37:37 -0700

    Spring boot project

---
+++ src/test/java/PersonalProjects/REST_API_CRUD/RestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package PersonalProjects.REST_API_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
