43772e91b72284d2cb0e0d36a63378957c1cacc2
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2024-07-01 19:55:55 -0700

    added PUT and DELETE requests so now we have CRUD operations

--- src/main/java/PersonalProjects/REST_API_CRUD/controller/CloudVendorAPIService.java
+++ src/main/java/PersonalProjects/REST_API_CRUD/controller/CloudVendorAPIService.java
@@ -1,8 +1,10 @@
 package PersonalProjects.REST_API_CRUD.controller;
 
 import PersonalProjects.REST_API_CRUD.model.CloudVendor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,22 @@ public class CloudVendorAPIService {
 //        "Address One", "xxxxx");
   }
 
-  @PostMapping
+  @PostMapping("/{vendorId}")
   public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
     this.cloudVendor = cloudVendor;
     return "Cloud Vendor Created Successfully";
+  }
+
+  @PutMapping("/{vendorId}")
+  public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+    this.cloudVendor = cloudVendor;
+    return "Cloud Vendor Updated Successfully";
+  }
+
+  @DeleteMapping("/{vendorId}")
+  public String deleteCloudVendorDetails(String vendorId){
+    this.cloudVendor = null;
+    return "Cloud Vendor Deleted Successfully";
   }
 
 }
